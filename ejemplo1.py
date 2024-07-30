22080b0ac556b005a1c0e58d4167609b18fd5025
Javier Curieses <jcurie2@yahoo.com.ar>
2024-07-30 15:33:55 -0300

    Agrego un print

--- ejemplo1.py
+++ ejemplo1.py
@@ -6,3 +6,5 @@ def sumar(n1,n2):
     print("El resultado de la suma entre n1 + n2 es de :", n1+n2)
 
 sumar(int(input("ingrese el primer numero")),int(input("ingrese el segundo numero")))
+
+print("otro git push")
